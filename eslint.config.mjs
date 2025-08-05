adfeffe8fba96d8870dca23316583ffef910686f
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2025-08-05 15:53:42 +0800

    chore(config): update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,12 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+import jestPlugin from 'eslint-plugin-jest';
+import importPlugin from 'eslint-plugin-import';
+import eslintParserTs from '@typescript-eslint/parser';
+import eslintPluginTs from '@typescript-eslint/eslint-plugin';
+import unusedImportsPlugin from 'eslint-plugin-unused-imports';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +16,60 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('eslint-config-next'),
+  {
+    ignores: ['**/node_modules/**', '**/out/*', '**/.next/*', 'next.config.js'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      globals: {
+        ...jestPlugin.environments.globals.globals,
+      },
+      parser: eslintParserTs,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      jest: jestPlugin,
+      import: importPlugin,
+      '@typescript-eslint': eslintPluginTs,
+      'unused-imports': unusedImportsPlugin,
+    },
+    rules: {
+      'no-undef': 'error',
+      'no-console': 'warn',
+      '@next/next/no-html-link-for-pages': 'off',
+      'unused-imports/no-unused-imports': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          'newlines-between': 'always',
+        },
+      ],
+      'unused-imports/no-unused-vars': [
+        'warn',
+        { vars: 'all', varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
