8dff8538889db4c1e04f6a3916bc90826deb5789
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2025-08-06 09:23:43 +0800

    test: test env variable & intellisense

--- __tests__/app/route.test.ts
+++ __tests__/app/route.test.ts
@@ -2,6 +2,11 @@ import { NextResponse } from 'next/server';
 
 import { GET } from '@/app/route';
 
+// ----------------------------------------------------------------------
+
+// Mock environment variable for testing
+process.env.NEXT_PUBLIC_HOST_URL = 'http://localhost:3000';
+
 describe('GET /api', () => {
   it("returns 'Hello world!' JSON response", async () => {
     const response = await GET();
@@ -10,6 +15,6 @@ describe('GET /api', () => {
 
     const json = await response.json();
 
-    expect(json).toEqual({ message: 'Hello world!' });
+    expect(json).toEqual({ message: 'Hello world! http://localhost:3000' });
   });
 });
