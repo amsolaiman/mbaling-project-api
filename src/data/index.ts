07f0fb9a33699adaba26311b9aec2113441961ce
moizsolaiman <abdul.solaiman@accessgroup.net.au>
2025-08-07 10:33:05 +0800

    chore: add type interfaces of users data (#13)
    * chore: add type interfaces of users data
    
    * chore: apply interfaces to json data

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,3 +1,7 @@
+// types
+import { IUserItem } from '@/types/user';
+import { ILandlordDetail, IStudentDetail } from '@/types/detail';
+
 // students
 import studentUsersJson from './student-users.json';
 import studentDetailsJson from './student-details.json';
@@ -10,14 +14,13 @@ import landlordDetailsJson from './landlord-details.json';
 export const studentUsers = studentUsersJson.map((user) => ({
   ...user,
   avatarUrl: `${process.env.NEXT_PUBLIC_HOST_URL}${user.avatarUrl}`,
-}));
+})) as Array<IUserItem>;
+
+export const studentDetails = studentDetailsJson as Array<IStudentDetail>;
 
 export const landlordUsers = landlordUsersJson.map((user) => ({
   ...user,
   avatarUrl: `${process.env.NEXT_PUBLIC_HOST_URL}${user.avatarUrl}`,
-}));
+})) as Array<IUserItem>;
 
-export {
-  studentDetailsJson as studentDetails,
-  landlordDetailsJson as landlordDetails,
-};
+export const landlordDetails = landlordDetailsJson as Array<ILandlordDetail>;
