40b84ded2c243c95f7407805548816b9573adea9
moizsolaiman <abdul.solaiman@accessgroup.net.au>
2025-08-08 10:52:10 +0800

    chore: add post list & uploads data & assets (#19)
    * chore(assets): add post upload images
    
    * chore(data): add post list & uploads data

--- src/data/index.ts
+++ src/data/index.ts
@@ -4,6 +4,9 @@ import studentDetailsJson from './student-details.json';
 // landlords
 import landlordUsersJson from './landlord-users.json';
 import landlordDetailsJson from './landlord-details.json';
+// posts
+import postListJson from './post-list.json';
+import postUploadsJson from './post-uploads.json';
 
 // types
 import { IUserItem } from '@/types/user';
@@ -24,3 +27,10 @@ export const landlordUsers = landlordUsersJson.map((user) => ({
 })) as Array<IUserItem>;
 
 export const landlordDetails = landlordDetailsJson as Array<ILandlordDetail>;
+
+export const postList = postListJson;
+
+export const postUploads = postUploadsJson.map((upload) => ({
+  ...upload,
+  imageUrl: `${process.env.NEXT_PUBLIC_HOST_URL}${upload.imgUrl}`,
+}));
