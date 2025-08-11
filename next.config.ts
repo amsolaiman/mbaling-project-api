790bffe4d67fa9199574352c62d6498f72ceac1b
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2025-08-11 10:37:14 +0800

    feat: update .env schema w/ cors vars

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import { env, validateEnvClient } from './env.schema';
 
 const nextEnv = {
   NEXT_PUBLIC_HOST_URL: env.HOST_URL,
+  NEXT_PUBLIC_CORS_ORIGIN: env.CORS_ORIGIN,
+  NEXT_PUBLIC_CORS_METHODS: env.CORS_METHODS,
+  NEXT_PUBLIC_CORS_HEADERS: env.CORS_HEADERS,
 };
 
 validateEnvClient(nextEnv);
--- src/middleware.ts
+++ src/middleware.ts
@@ -6,9 +6,9 @@ import type { NextRequest } from 'next/server';
 export function middleware(_request: NextRequest) {
   const response = NextResponse.next();
 
-  response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  response.headers.set('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  response.headers.set('Access-Control-Allow-Origin', process.env.NEXT_PUBLIC_CORS_ORIGIN);
+  response.headers.set('Access-Control-Allow-Methods', process.env.NEXT_PUBLIC_CORS_METHODS);
+  response.headers.set('Access-Control-Allow-Headers', process.env.NEXT_PUBLIC_CORS_HEADERS);
 
   return response;
 }
