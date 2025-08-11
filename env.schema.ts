569a168e906925d70f7cbb61282e8e69bb5f91ad
moizsolaiman <abdul.solaiman@accessgroup.net.au>
2025-08-11 13:21:50 +0800

    feat(env): update env schema & validation w/ jwt auth vars (#26)
    * feat(env): update env schema & validation w/ jwt auth vars
    
    * chore: install jwt & bcrypt.js

--- env.schema.ts
+++ env.schema.ts
@@ -6,6 +6,16 @@ export const envSchema = Yup.object({
   // HOST
   HOST_URL: Yup.string().required('HOST_URL is required'),
 
+  // JWT TOKEN
+  JWT_SECRET_ACCESS_KEY: Yup.string().required('JWT_SECRET_ACCESS_KEY is required'),
+  JWT_SECRET_REFRESH_KEY: Yup.string().required('JWT_SECRET_REFRESH_KEY is required'),
+  JWT_ACCESS_EXPIRATION: Yup.string()
+    .matches(/^\d+[smhd]$/, 'JWT_ACCESS_EXPIRATION must be a number followed by s, m, h, or d')
+    .required('JWT_ACCESS_EXPIRATION is required'),
+  JWT_REFRESH_EXPIRATION: Yup.string()
+    .matches(/^\d+[smhd]$/, 'JWT_REFRESH_EXPIRATION must be a number followed by s, m, h, or d')
+    .required('JWT_REFRESH_EXPIRATION is required'),
+
   // CORS
   CORS_ORIGIN: Yup.string().required('CORS_ORIGIN is required'),
   CORS_METHODS: Yup.string()
@@ -25,6 +35,26 @@ export const envSchema = Yup.object({
 export const envClientSchema = Yup.object({
   // HOST
   NEXT_PUBLIC_HOST_URL: Yup.string().required('NEXT_PUBLIC_HOST_URL is required'),
+
+  // JWT TOKEN
+  NEXT_PUBLIC_JWT_SECRET_ACCESS_KEY: Yup.string().required(
+    'NEXT_PUBLIC_JWT_SECRET_ACCESS_KEY is required'
+  ),
+  NEXT_PUBLIC_JWT_SECRET_REFRESH_KEY: Yup.string().required(
+    'NEXT_PUBLIC_JWT_SECRET_REFRESH_KEY is required'
+  ),
+  NEXT_PUBLIC_JWT_ACCESS_EXPIRATION: Yup.string()
+    .matches(
+      /^\d+[smhd]$/,
+      'NEXT_PUBLIC_JWT_ACCESS_EXPIRATION must be a number followed by s, m, h, or d'
+    )
+    .required('NEXT_PUBLIC_JWT_ACCESS_EXPIRATION is required'),
+  NEXT_PUBLIC_JWT_REFRESH_EXPIRATION: Yup.string()
+    .matches(
+      /^\d+[smhd]$/,
+      'NEXT_PUBLIC_JWT_REFRESH_EXPIRATION must be a number followed by s, m, h, or d'
+    )
+    .required('NEXT_PUBLIC_JWT_REFRESH_EXPIRATION is required'),
 
   // CORS
   NEXT_PUBLIC_CORS_ORIGIN: Yup.string().required('NEXT_PUBLIC_CORS_ORIGIN is required'),
