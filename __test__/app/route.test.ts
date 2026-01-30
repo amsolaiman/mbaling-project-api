bf245e0cfda578ab7babdc86e71f836cebe6016d
moizsolaiman <abdul.solaiman@accessgroup.net.au>
2026-01-30 15:14:50 +0800

    chore: apply jest (#2)

---
+++ __test__/app/route.test.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+import { GET } from "@/app/route";
+
+describe("GET /api", () => {
+  it("returns 'Hello world!' JSON response", async () => {
+    const response = await GET();
+
+    expect(response).toBeInstanceOf(NextResponse);
+
+    const json = await response.json();
+
+    expect(json).toEqual({ message: "Hello world!" });
+  });
+});
