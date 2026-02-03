19a830f6a0ab4535bf5d962e7c5f12d4613dd354
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2026-02-03 14:29:24 +0800

    test: surface landlord users list

--- src/app/route.ts
+++ src/app/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-import { studentUsers as users } from '@/data';
+import { landlordUsers as users } from '@/data';
 
 // ----------------------------------------------------------------------
 
