5f601cccac509680ee25f51958971bd6b9113b7b
moizsolaiman <abdul.solaiman@accessgroup.net.au>
2026-02-03 14:33:52 +0800

    feat: add landlord users data & types (#17)
    * feat: add landlord users data & types
    
    * test: surface landlord users list

--- __test__/app/route.test.ts
+++ __test__/app/route.test.ts
@@ -3,18 +3,18 @@ import { NextResponse } from 'next/server';
 // route
 import { GET } from '@/app/route';
 // data
-import { studentUsers } from '@/data';
+import { landlordUsers } from '@/data';
 
 // ----------------------------------------------------------------------
 
 describe('GET /api', () => {
-  it('returns JSON response of Student users', async () => {
+  it('returns JSON response of Landlord users', async () => {
     const response = await GET();
 
     expect(response).toBeInstanceOf(NextResponse);
 
     const json = await response.json();
 
-    expect(json).toEqual(studentUsers);
+    expect(json).toEqual(landlordUsers);
   });
 });
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,11 +1,22 @@
+// types
 import { IUserItem } from '@/types/user';
-import { IStudentDetail } from '@/types/detail';
+import { ILandlordDetail, IStudentDetail } from '@/types/detail';
 
 // students
 import studentUsersJson from './student-users.json';
 import studentDetailsJson from './student-details.json';
+// landlords
+import landlordUsersJson from './landlord-users.json';
+import landlordDetailsJson from './landlord-details.json';
 
 // ----------------------------------------------------------------------
+
+export const landlordUsers = landlordUsersJson.map((user) => ({
+  ...user,
+  avatarUrl: `${process.env.NEXT_PUBLIC_HOST_URL}${user.avatarUrl}`,
+})) as Array<IUserItem>;
+
+export const landlordDetails = landlordDetailsJson as Array<ILandlordDetail>;
 
 export const studentUsers = studentUsersJson.map((user) => ({
   ...user,
