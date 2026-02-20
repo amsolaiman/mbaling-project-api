04cbd12eae6fc621882f2684d8b5968af11b97ac
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2026-02-20 08:37:40 +0800

    feat: display post api routes to landing page

--- src/sections/_config-route.ts
+++ src/sections/_config-route.ts
@@ -60,4 +60,47 @@ export const routeConfig: RouteConfig[] = [
       },
     ],
   },
+  {
+    header: 'Post',
+    children: [
+      {
+        title: 'Post List',
+        type: 'get',
+        caption: 'Get all posts',
+        path: '/api/post/list',
+      },
+      {
+        title: 'Post Details',
+        type: 'get',
+        caption: 'Get post details by id',
+        path: '/api/post/list/<strong>:id</strong>',
+      },
+      {
+        title: 'Post List Paginated',
+        type: 'get',
+        caption: 'Get list of posts by pagination params',
+        path: '/api/post/list?page=<strong>{page}</strong>&limit=<strong>{limit}</strong>',
+        note: 'Parameters can be used with post user & search routes',
+      },
+      {
+        title: 'Post List by Landlord',
+        type: 'get',
+        caption: 'Get all posts by landlord user',
+        path: '/api/post/user?id=<strong>{id}</strong>',
+      },
+      {
+        title: 'Post Search',
+        type: 'get',
+        caption: 'Search post',
+        path: '/api/post/search?query=<strong>{query}</strong>',
+      },
+      {
+        title: 'Post with Landlord Details',
+        type: 'get',
+        caption: 'Get post data w/ landlord details',
+        path: '/api/post/<strong>[route]</strong>?createdBy=true',
+        note: 'Parameter can be used with any post routes',
+      },
+    ],
+  },
 ];
