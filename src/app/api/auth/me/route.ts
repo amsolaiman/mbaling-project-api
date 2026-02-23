f82bc8a2be5529ec154cd31e872e8b379d31a330
moizsolaiman <abdul.solaiman@accessgroup.net.au>
2026-02-23 15:42:14 +0800

    feat: add auth me api route (#35)

---
+++ src/app/api/auth/me/route.ts
@@ -0,0 +1,67 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+// utils
+import { omit } from '@/utils/object';
+import { authenticateAccessToken } from '@/utils/auth';
+// data
+import {
+  landlordDetails,
+  landlordUsers,
+  studentDetails,
+  studentUsers,
+} from '@/data';
+// types
+import { ILandlordDetail, IStudentDetail } from '@/types/detail';
+import { UserLandlordResponse, UserStudentResponse } from '@/types/user';
+
+// ----------------------------------------------------------------------
+
+export async function GET(request: NextRequest) {
+  const auth = await authenticateAccessToken(request);
+
+  if (auth instanceof NextResponse) {
+    return auth;
+  }
+
+  const userAuth = auth as { id: string; email: string };
+
+  const userList = [...landlordUsers, ...studentUsers];
+
+  //#region Checking user
+  const foundUser = userList.find((user) => user.id === userAuth.id);
+
+  if (!foundUser) {
+    return NextResponse.json({ message: 'User not found.' }, { status: 404 });
+  }
+  //#endregion
+
+  if (foundUser.role === 'landlord') {
+    const detail =
+      landlordDetails.find((detail) => detail.userId === foundUser.id) ||
+      ({} as ILandlordDetail);
+
+    const result: UserLandlordResponse = {
+      ...omit(foundUser, ['password']),
+      details: omit(detail, ['userId']),
+    };
+
+    return NextResponse.json({ user: result }, { status: 200 });
+  }
+
+  if (foundUser.role === 'student') {
+    const detail =
+      studentDetails.find((detail) => detail.userId === foundUser.id) ||
+      ({} as IStudentDetail);
+
+    const result: UserStudentResponse = {
+      ...omit(foundUser, ['password']),
+      details: omit(detail, ['id', 'userId']),
+    };
+
+    return NextResponse.json({ user: result }, { status: 200 });
+  }
+
+  const result = omit(foundUser, ['password']);
+
+  return NextResponse.json({ user: result }, { status: 200 });
+}
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,5 +1,7 @@
 import jwt from 'jsonwebtoken';
+import { NextRequest, NextResponse } from 'next/server';
 
+// types
 import { IUserItem } from '@/types/user';
 
 // ----------------------------------------------------------------------
@@ -10,4 +12,26 @@ export function generateAccessToken(user: IUserItem) {
   const payload = { id: user.id, email: user.email };
 
   return jwt.sign(payload, SECRET_KEY);
+}
+
+export async function authenticateAccessToken(request: NextRequest) {
+  const token = request.headers.get('Authorization')?.split(' ')[1];
+
+  if (!token) {
+    return NextResponse.json(
+      { message: 'No token provided.' },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+
+    return decoded;
+  } catch {
+    return NextResponse.json(
+      { message: 'Invalid or expired token.' },
+      { status: 401 }
+    );
+  }
 }
