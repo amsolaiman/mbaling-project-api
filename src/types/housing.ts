35843653019d1633969f9413bcb6c29f1b2b576a
moizsolaiman <abdul.solaiman@accessgroup.net.au>
2026-03-09 13:32:05 +0800

    feat: add applicant list-by-landlord api route (#56)
    * feat: add applicant list-by-landlord api route
    
    * chore: add deletedAt & deletedBy to application list data

--- src/types/housing.ts
+++ src/types/housing.ts
@@ -1,3 +1,5 @@
+import { UserStudentResponse } from './user';
+
 // ----------------------------------------------------------------------
 
 export interface IApplicationItem {
@@ -5,4 +7,10 @@ export interface IApplicationItem {
   housingId: string;
   createdAt: Date | string;
   createdBy: string;
+  deletedAt: Date | string | null;
+  deletedBy: string | null;
+}
+
+export interface HousingApplicantResponse extends IApplicationItem {
+  studentDetails: UserStudentResponse;
 }
