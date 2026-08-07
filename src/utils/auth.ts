6d3b06627b66b19a73e98b15b71fbbe0ae47d2b1
Moiz--Access <abdul.solaiman@accessgroup.net.au>
2026-08-07 08:05:57 +0800

    feat: add 1-hr access token expiration to auth login (#67)

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -11,7 +11,7 @@ const SECRET_KEY = process.env.JWT_SECRET_ACCESS_KEY as string;
 export function generateAccessToken(user: IUserItem) {
   const payload = { id: user.id, email: user.email };
 
-  return jwt.sign(payload, SECRET_KEY);
+  return jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
 }
 
 export async function authenticateAccessToken(request: NextRequest) {
